c57360532e43fb3740aeeb9ea884cac01b190686
Vemrs <65913033+Vemrs@users.noreply.github.com>
2024-05-06 10:25:55 +0100

    Add files via upload
    javascript

---
+++ no1.js
@@ -0,0 +1,14 @@
+let x = 5;
+let y = 3;
+let person = { name: 'John', age: 30 };
+console.log(person.name); 
+if (x > y) {
+    console.log('x is greater than y');
+ } else {
+     console.log('x is less than or equal to y');
+}
+let add= function(x,y){
+    return x+y;
+}
+let result=add(2,3);
+console.log(result);
